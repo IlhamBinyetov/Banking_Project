9f73ec11535c33e2566ab359e921a7721e887e0b
Ilham Binyetov <ilhamgb@code.edu.az>
2024-04-05 12:19:05 +0400

    Change the Id Type Of AspNetUsers and Roles Table

---
+++ Banking_Project.DataAccessLayer/Concrete/Context.cs
@@ -0,0 +1,22 @@
+﻿using Banking_Project.EntityLayer.Concrete;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Banking_Project.DataAccessLayer.Concrete
+{
+    public class Context : IdentityDbContext<AppUser,AppRole,int>
+    {
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=DESKTOP-1AOIQON;initial catalog=BankingProjectDB;integrated Security=true");
+        }
+
+        public DbSet<CustomerAccount> CustomerAccounts { get; set; }
+        public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+    }
+}
