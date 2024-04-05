54ff18987305bff23fbb7737c3f29ddb2f2bdf29
Ilham Binyetov <ilhamgb@code.edu.az>
2024-04-05 12:42:53 +0400

    Add new class to DtoLayer

--- Banking_Project.EntityLayer/Concrete/CustomerAccount.cs
+++ Banking_Project.EntityLayer/Concrete/CustomerAccount.cs
@@ -13,5 +13,7 @@ namespace Banking_Project.EntityLayer.Concrete
         public string CustomerAccountCurrency { get; set; }
         public decimal CustomerAccountBalance { get; set; }
         public string BankBranch { get; set; }
+        public int AppUserId { get; set; }
+        public AppUser AppUser { get; set; }
     }
 }
