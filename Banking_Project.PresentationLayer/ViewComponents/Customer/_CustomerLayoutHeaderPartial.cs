be9a6d2470463a868e655084684e3144a7e57a7f
Ilham Binyetov <ilhamgb@code.edu.az>
2024-04-09 18:18:00 +0400

    Added  the views of some pages and create some folders for viewComponents

---
+++ Banking_Project.PresentationLayer/ViewComponents/Customer/_CustomerLayoutHeaderPartial.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Dynamic;
+
+namespace Banking_Project.PresentationLayer.ViewComponents.Customer
+{
+    public class _CustomerLayoutHeaderPartial : ViewComponent
+    {
+        public IViewComponentResult Invoke() 
+        { 
+            return View(); 
+        }
+    }
+}
