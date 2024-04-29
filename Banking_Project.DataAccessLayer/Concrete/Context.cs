3b717324bddfa3634186a6ce1569d0c92719dd06
Ilham Binyetov <ilhamgb@code.edu.az>
2024-04-29 16:22:11 +0400

    Add new Properties to Customer Account and Customer Account Process Table

--- Banking_Project.DataAccessLayer/Concrete/Context.cs
+++ Banking_Project.DataAccessLayer/Concrete/Context.cs
@@ -14,9 +14,28 @@ namespace Banking_Project.DataAccessLayer.Concrete
         protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
         {
             optionsBuilder.UseSqlServer("server=DESKTOP-1AOIQON;initial catalog=BankingProjectDB;integrated Security=true");
+            // optionsBuilder.UseSqlServer("Data Source=sql_server,1933;Initial Catalog=BankingProjectDB;User Id=sa;Password=StrongPassword123");
+
         }
 
         public DbSet<CustomerAccount> CustomerAccounts { get; set; }
         public DbSet<CustomerAccountProcess> CustomerAccountProcesses { get; set; }
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            builder.Entity<CustomerAccountProcess>()
+                .HasOne(x => x.SenderCustomer)
+                .WithMany(y => y.CustomerSender)
+                .HasForeignKey(z => z.SenderId)
+                .OnDelete(DeleteBehavior.ClientSetNull);
+
+
+            builder.Entity<CustomerAccountProcess>()
+            .HasOne(x => x.ReceiverCustomer)
+            .WithMany(y => y.CustomerReceiver)
+            .HasForeignKey(z => z.ReceiverId)
+            .OnDelete(DeleteBehavior.ClientSetNull);
+
+            base.OnModelCreating(builder);
+        }
     }
 }
--- Banking_Project.DataAccessLayer/Migrations/20240429120300__add_new_properties_to_CustomerAccount_table.Designer.cs
+++ Banking_Project.DataAccessLayer/Migrations/20240429120300__add_new_properties_to_CustomerAccount_table.Designer.cs
@@ -4,6 +4,7 @@ using Banking_Project.DataAccessLayer.Concrete;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 #nullable disable
@@ -11,9 +12,10 @@ using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 namespace Banking_Project.DataAccessLayer.Migrations
 {
     [DbContext(typeof(Context))]
-    partial class ContextModelSnapshot : ModelSnapshot
+    [Migration("20240429120300__add_new_properties_to_CustomerAccount_table")]
+    partial class _add_new_properties_to_CustomerAccount_table
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
